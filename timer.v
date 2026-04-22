fd50d12455cf6029b9422e972b2e380e5e87e750
Michelle Joo <michellejoo@u.boisestate.edu>
2026-04-22 16:41:07 -0600

    Update timer.v

--- timer.v
+++ timer.v
@@ -5,9 +5,21 @@ module timer(
     input en,               //Enables or Disables clock
     input load,             //If load=1, load the counter with "load_value"
     input [5:0] load_value, //Value to load into counter register. Counter will then start counting from this value
-    output [5:0] state     //6-bits to represent the highest number 59
+    output reg [5:0] state     //6-bits to represent the highest number 59
 );
-
-
+    always @#(posedge clk or posedge rst) begin
+        if (rst)
+            state <=6'd0;
+        else if (load)
+            state <= load_value;
+        else if (en) begin
+            if (state == 6'd0)
+                state <= 6'd0;
+            else
+                state <= state - 6'd1;
+        end
+        else
+            state <= state;
+    end
 
 endmodule
