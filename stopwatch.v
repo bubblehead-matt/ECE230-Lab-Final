243c05fa0451833b40a71ef7b4d2aeffc96a8649
Michelle Joo <michellejoo@u.boisestate.edu>
2026-04-22 16:26:28 -0600

    Update stopwatch.v

--- stopwatch.v
+++ stopwatch.v
@@ -3,9 +3,20 @@ module stopwatch(
     input clk,
     input rst,
     input en,
-    output [5:0] state     //6-bits to represent the highest number 59
+    output reg [5:0] state     //6-bits to represent the highest number 59
 );
-    
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            state <= 6'd0;
+        else if (en) begin
+            if (state == 6'd59)
+                state <= 6'd0;
+            else
+                state <= state + 6'd1;
+        end
+        else
+            state <= state;
+    end
    
 endmodule
 
